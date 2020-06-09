6ea12c73480843ec02c5e980e76f031e40ad5873
jimmygranadino <jimmy.granadino@gmail.com>
2020-06-09 09:48:50 -0700

    established win conditions

--- js/app.js
+++ js/app.js
@@ -1,36 +1,47 @@
-var playArea = document.getElementsByClassName("playArea")
-var playerOne = document.getElementById("player1Score")
-var playerTwo = document.getElementById("player2Score")
-//player1 tally
-//player2 tally
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("my page loaded.")
 
-/* ==== VARIABLES ==== */
-
-let spacesFilled = {
-    a1: false
-    a2: false
-    a3: false
-    b1: false
-    b2: false
-    b3: false
-    c1: false
-    c2: false
-    c3: false
-}
-
-//variables for how to win here
-
-//player1 win tally
-
-//player2 win tally
-
-//variable for tracking player turn
-
-//variable game is won = boolean true or false -- then run against whether tie or actually lost
+    /* ---- Set play area, score tally and move indicators --- */
+    let playArea = document.getElementsByClassName("playArea")
+    let playerOne = document.getElementById("player1Score")
+    let playerTwo = document.getElementById("player2Score")
+    let indicatorX = "X"
+    let indicatorO = "O"
 
 
-/* EVENT LISTENERS */
+    /* ---- PLAY AREA VARIABLES, all set to false for new game ---- */
+    let newGame =[
+        a1 = false,
+        a2 = false,
+        a3 = false,
+        b1 = false,
+        b2 = false,
+        b3 = false,
+        c1 = false,
+        c2 = false,
+        c3 = false
+    ]
 
-// event listener click for computer player or human players
+    /* ---- EVERY WIN CONDITION, check against array ---- */
 
-playArea.addEventlistener("click", spacesClick)
+    var winConditions = [
+        [a1, a2, a3],
+        [b1, b2, b3],
+        [c1, c2, c3],
+        [a1, b1, c1],
+        [a2, b2, c2],
+        [a3, b3, c3],
+        [a1, b2, c3],
+        [a3, b2, c1]
+    ]
+
+    //variable for tracking player turn
+
+    //variable game is won = boolean true or false -- then run against whether tie or actually lost
+
+    // place indicator in play area
+
+    // begin new game
+
+
+})
