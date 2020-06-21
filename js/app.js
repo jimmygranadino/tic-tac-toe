78eec453b2c5fa07ff6c7b1ea6c7fc3846f8cd09
jimmygranadino <jimmy.granadino@gmail.com>
2020-06-21 13:42:56 -0700

    complete

--- js/app.js
+++ js/app.js
@@ -1,87 +1,81 @@
-console.log("load test")
-
-/* ---- Set play area, score tally and move indicators --- */
-let playerX = document.querySelectorAll("#playerXTally")
-let playerO = document.querySelectorAll("#playerOTally")
-let indicatorX = "X"
-let indicatorO = "O"
-let turnMechanism = 0
-let currentPlayer = indicatorX
-let player = indicatorX
-let turnIndicator = document.querySelectorAll("#turnIndicator")
-
-/* ---- PLAY AREA VARIABLES, all set to false for new game ---- */
-let newGame =[
-    a1 = false,
-    a2 = false,
-    a3 = false,
-    b1 = false,
-    b2 = false,
-    b3 = false,
-    c1 = false,
-    c2 = false,
-    c3 = false
+// all win conditions
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
 ]
 
-/* ---- EVERY WIN CONDITION, check against array ---- */
 
-var winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+// helper function that grabs the array
+const grid = () => Array.from(document.getElementsByClassName("q"))
+console.log("1) "+ grid)
 
-//variable for tracking player turn
+// converts the cell from a string to a number
+const qNumId = (qE1) => Number.parseInt(qE1.id.replace("q",""))
+console.log("2) " + qNumId)
 
-function playerTurn(){
-    // if turn mechanism is 0 the player X goes
-    // innerText that its player X turn
-    // else statement because anything other than 0 means its player O turn 
-    // innerText that its player O turn
+// return which cells are empty
+const emptyQs = () => grid().filter(_qE1 => _qE1.innerText === "")
+console.log("3) " + emptyQs)
+
+// are all items in the array the same that aren't blanks
+const allSame = (arr) => arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '')
+console.log("4) " + allSame)
+
+const takeTurn = (index, letter) => grid()[index].innerText = letter
+const opponentChoice = () => qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)])
+
+// end game states
+const endGame = (winningSequence) => {
+  winningSequence.forEach(_qE1 => _qE1.classList.add("winner"))
+}
+const checkForVictory = () => {
+  let victory = false
+
+  winningCombos.forEach(_c => {
+      const _grid = grid()
+      const sequence = [_grid[_c[0]], _grid[_c[1]], _grid[_c[2]]]
+      if(allSame(sequence)) {
+          victory = true
+          endGame(sequence)
+      }
+  })
+
+  return victory
 }
 
-console.log(currentPlayer)
-console.log(turnMechanism)
-console.log(player)
 
+// opponent takes turn
+const opponentTurn = () => {
+  disableListeners()
+  document.getElementById("turnDisplay").innerHTML = "PLAYER 2 IS MAKING THEIR MOVE. PLEASE WAIT..."
+  setTimeout(() => {
+      takeTurn(opponentChoice(), "O")
+      document.getElementById("turnDisplay").innerHTML = "Player 1, place your X in a space."
+      if(!checkForVictory())
+          enableListeners()
+  }, 1000)
+}
 
-// variable game is won = then run against whether tie or actually lost
+// click function for player
+const clickFn = ($event) => {
+  takeTurn(qNumId($event.target), "X")
+  if(!checkForVictory())
+      opponentTurn()
+}
 
-// place indicator in play area
+// turn on click for player turn
+const enableListeners = function() {
+  grid().forEach(_qE1 => _qE1.addEventListener("click", clickFn))
+}
 
-// start game / new game runs a function that enables the listener for clicking
+// turn off click when not player turn
+const disableListeners = () => grid().forEach(_qE1 => _qE1.removeEventListener("click", clickFn))
 
-// still figuring out why event listeners kept breaking with classes playArea and .spaces so DRY has been ignored while I keep building/testing
-
-document.getElementById("a1").addEventListener("click", function() {
-    console.log("a1")
-  });
-document.getElementById("a2").addEventListener("click", function() {
-    console.log("a2")
-  });
-document.getElementById("a3").addEventListener("click", function() {
-    console.log("a3")
-  });
-document.getElementById("b1").addEventListener("click", function() {
-    console.log("b1")
-  });
-document.getElementById("b2").addEventListener("click", function() {
-    console.log("b2")
-  });
-document.getElementById("b3").addEventListener("click", function() {
-    console.log("b3")
-  });
-document.getElementById("c1").addEventListener("click", function() {
-    console.log("c1")
-  });
-document.getElementById("c2").addEventListener("click", function() {
-    console.log("c2")
-  });
-document.getElementById("c3").addEventListener("click", function() {
-    console.log("c3")
-  });
+// calls the function
+enableListeners()
